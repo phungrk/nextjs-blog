6ee411744bffaadf278c7176ef6df3ed118a0a4c
Nguyen Cao Phung <phungnc@gmail.com>
2022-08-03 16:47:49 +0700

    add slicemachine

--- components/layout.tsx
+++ components/layout.tsx
@@ -1,13 +1,13 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import styles from './layout.module.css'
-import utilStyles from '../styles/utils.module.css'
-import Link from 'next/link'
+import Head from "next/head";
+import Image from "next/image";
+import styles from "./layout.module.css";
+import utilStyles from "../styles/utils.module.css";
+import Link from "next/link";
 
-const name = '[Your Name]'
-export const siteTitle = 'Next.js Sample Website'
+const name = "[Your Name]";
+export const siteTitle = "Next.js Sample Website";
 
-export default function Layout({ children, home }) {
+export default function Layout({ children, home = false }) {
   return (
     <div className={styles.container}>
       <Head>
@@ -69,5 +69,5 @@ export default function Layout({ children, home }) {
         </div>
       )}
     </div>
-  )
+  );
 }
---
+++ pages/_app.tsx
@@ -0,0 +1,28 @@
+import { AppProps } from "next/app";
+import "../styles/global.css";
+import Link from "next/link";
+import { PrismicProvider } from "@prismicio/react";
+import { PrismicPreview } from "@prismicio/next";
+import { linkResolver, repositoryName } from "../prismicio";
+
+// function App({ Component, pageProps }: AppProps) {
+//   return <Component {...pageProps} />;
+// }
+function App({ Component, pageProps }: AppProps) {
+  return (
+    <PrismicProvider
+      linkResolver={linkResolver}
+      internalLinkComponent={({ href, children, ...props }) => (
+        <Link href={href}>
+          <a {...props}>{children}</a>
+        </Link>
+      )}
+    >
+      <PrismicPreview repositoryName={repositoryName}>
+        <Component {...pageProps} />
+      </PrismicPreview>
+    </PrismicProvider>
+  );
+}
+
+export default App;
